b87ea3295d57dab25fcffb9f0c2cdf40970b7fe8
Matheus <matheus.araujo.net@hotmail.com>
2022-03-11 03:12:36 -0300

    Entidade Order, datas com instant e padrão ISO 8601

---
+++ src/main/java/com/estudosmatheus/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.estudosmatheus.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudosmatheus.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
